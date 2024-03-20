a7975385ba5e7a5397c9c485220631b217a1b0c3
Bookworm2035 <126933809+Bookworm2035@users.noreply.github.com>
2024-03-20 14:05:23 -0600

    Scalfold

---
+++ Easton_Mission11_Amazon/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Easton_Mission11_Amazon.Models;
+
+public partial class Book
+{
+    public int BookId { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Author { get; set; } = null!;
+
+    public string Publisher { get; set; } = null!;
+
+    public string Isbn { get; set; } = null!;
+
+    public string Classification { get; set; } = null!;
+
+    public string Category { get; set; } = null!;
+
+    public int PageCount { get; set; }
+
+    public double Price { get; set; }
+}
