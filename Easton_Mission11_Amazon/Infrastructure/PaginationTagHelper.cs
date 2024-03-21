f0bf0b9d2c7bf5c0a08b3ae54aa6dcb303cefed6
Bookworm2035 <126933809+Bookworm2035@users.noreply.github.com>
2024-03-21 16:55:05 -0600

    it works

--- Easton_Mission11_Amazon/Infrastructure/PaginationTagHelper.cs
+++ Easton_Mission11_Amazon/Infrastructure/PaginationTagHelper.cs
@@ -23,6 +23,13 @@ namespace Easton_Mission11_Amazon.Infrastructure
         public string? PageAction { get; set; }
         public PaginationInfo PageModel {  get; set; }
 
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; } = String.Empty;
+
+        public string PageClassNormal {  get; set; } = String.Empty;
+
+        public string PageClassSelected {  get; set; } = String.Empty;
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (ViewContext != null && PageModel != null)
@@ -36,7 +43,15 @@ namespace Easton_Mission11_Amazon.Infrastructure
                     TagBuilder tag = new TagBuilder("a");
 
                     tag.Attributes["href"] = urlHelper.Action(PageAction, new { pageNum = i });
+                    if (PageClassesEnabled) { 
+                        tag.AddCssClass(PageClass);
+                        tag.AddCssClass(i==PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                    }
+
                     tag.InnerHtml.Append(i.ToString());
+
+                    // Append a space after each page number
+                    //tag.InnerHtml.Append(" ");
 
                     result.InnerHtml.AppendHtml(tag);
                 }
