5e05ab8a53a2f75885e169d241fa71f7c9fa9a32
Bookworm2035 <126933809+Bookworm2035@users.noreply.github.com>
2024-03-20 22:42:38 -0600

    tag help?

---
+++ Easton_Mission11_Amazon/Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Runtime.CompilerServices;
+using Easton_Mission11_Amazon.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+namespace Easton_Mission11_Amazon.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes="page-model")]
+    public class PaginationTagHelper:TagHelper
+    {
+        private IUrlHelperFactory urlHelperFactory;
+
+        public PaginationTagHelper (IUrlHelperFactory temp)
+        {
+            urlHelperFactory = temp;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext? ViewContext { get; set; }
+        public string? PageAction { get; set; }
+        public PaginationInfo PageModel {  get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (ViewContext != null && PageModel != null)
+            {
+                IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+
+                TagBuilder result = new TagBuilder("div");
+
+                for (int i = 1; i <= PageModel.TotalPages; i++)
+                {
+                    TagBuilder tag = new TagBuilder("a");
+
+                    tag.Attributes["href"] = urlHelper.Action(PageAction, new { pageNum = i });
+                    tag.InnerHtml.Append(i.ToString());
+
+                    result.InnerHtml.AppendHtml(tag);
+                }
+                
+                output.Content.AppendHtml(result.InnerHtml);
+
+            }
+        }
+    }
+}
